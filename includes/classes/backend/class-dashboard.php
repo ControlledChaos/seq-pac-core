f458efec97359e0355dae9b6e147c19f505aeb92
ControlledChaos <greg@ccdzine.com>
2021-05-13 15:24:36 -0700

    Custom admin welcome panel.

--- includes/classes/backend/class-dashboard.php
+++ includes/classes/backend/class-dashboard.php
@@ -32,6 +32,19 @@ class Dashboard extends Classes\Base {
 
 		// Remove widgets.
 		add_action( 'wp_dashboard_setup', [ $this, 'remove_widgets' ] );
+
+		// Print styles & scripts to the head.
+		add_action( 'admin_head', [ $this, 'admin_head' ] );
+
+		// Custom Welcome panel.
+		remove_action( 'welcome_panel', 'wp_welcome_panel' );
+		add_action( 'welcome_panel', [ $this, 'welcome_panel' ], 25 );
+
+		// Enqueue dashboard assets.
+		add_action( 'admin_enqueue_scripts', [ $this, 'assets' ] );
+
+		// Add custom post types & taxonomies to the summary.
+		add_action( 'dashboard_glance_items', [ $this, 'summary' ] );
 	}
 
 	/**
@@ -52,6 +65,16 @@ class Dashboard extends Classes\Base {
 		// ClassicPress petitions.
 		unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_petitions'] );
 
+		/**
+		 * Hide At a Glance for those that can edit theme options because
+		 * this is the capability needed to run the `welcome_panel` hook.
+		 * So this will display At a Glance to those who cannot view the
+		 * welcome panel.
+		 */
+		if ( current_user_can( 'edit_theme_options' ) ) {
+			unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now'] );
+		}
+
 		// Site Health.
 		if ( defined( 'SPRC_ALLOW_SITE_HEALTH' ) && ! SPRC_ALLOW_SITE_HEALTH ) {
 			remove_meta_box( 'dashboard_site_health', 'dashboard', 'normal' );
@@ -59,5 +82,142 @@ class Dashboard extends Classes\Base {
 
 		// PHP update nag.
 		unset( $wp_meta_boxes['dashboard']['normal']['high']['dashboard_php_nag'] );
+	}
+
+	/**
+	 * Enqueue admin scripts
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function assets() {
+
+		// Get the screen ID to target the Dashboard.
+		$screen = get_current_screen();
+
+		// Enqueue only on the Dashboard screen.
+		if ( $screen->id == 'dashboard' ) {
+			wp_enqueue_style( SPRC_CONFIG['admin_slug'] . '-welcome', SPRC_URL .  'assets/css/welcome.min.css', [], null, 'screen' );
+		}
+	}
+
+	/**
+	 * Get the custom Welcome panel
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function welcome_panel() {
+
+		// Look first in the active theme for a welcome panel template.
+		$welcome = locate_template( 'template-parts/admin/welcome-panel.php' );
+
+		if ( ! empty( $welcome ) ) {
+			get_template_part( 'template-parts/admin/welcome-panel' );
+		} else {
+			include_once SPRC_PATH . 'views/backend/widgets/welcome-panel.php';
+		}
+	}
+
+	/**
+	 * Print styles & scripts to the head
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return string Returns the styles & scripts.
+	 */
+	public function admin_head() {
+
+		$print = '
+			<style>
+				/*
+				* Welcome panel user dismiss option
+				* is disabled in the Customizer
+				*/
+				a.welcome-panel-close, #wp_welcome_panel-hide, .metabox-prefs label[for="wp_welcome_panel-hide"] {
+					display: none !important;
+				}
+				.welcome-panel {
+					display: block !important;
+				}
+			</style>
+			';
+		$print .= '
+			<script></script>
+			';
+
+		/**
+		 * If the At a Glance widget is removed,
+		 * don't display the widgets container.
+		 */
+		if ( current_user_can( 'edit_theme_options' ) ) {
+			$print .= '
+			<style>#dashboard-widgets { display: none; }</style>
+			';
+		}
+
+		// Print the styles & scripts.
+		echo $print;
+	}
+
+	/**
+	 * Add custom post types to dashboard summary
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function summary() {
+
+		// Post type query arguments.
+		$args = [
+			'public'   => true,
+			'_builtin' => false
+		];
+
+		// The type of output to return, either 'names' or 'objects'.
+		$output = 'object';
+
+		// The operator (and/or) to use with multiple $args.
+		$operator = 'and';
+
+		// Get post types according to above.
+		$post_types = get_post_types( $args, $output, $operator );
+
+		// Prepare an entry for each post type matching the query.
+		foreach ( $post_types as $post_type ) {
+
+			// Count the number of posts.
+			$count = wp_count_posts( $post_type->name );
+
+			// Get the number of published posts.
+			$number = number_format_i18n( $count->publish );
+
+			// Get the plural or single name based on the count.
+			$name = _n( $post_type->labels->singular_name, $post_type->labels->name, intval( $count->publish ) );
+
+			// Supply an edit link if the user can edit posts.
+			if ( current_user_can( 'edit_posts' ) ) {
+				echo sprintf(
+					'<li class="post-count %1s-count"><a href="edit.php?post_type=%2s">%3s %4s</a></li>',
+					$post_type->name,
+					$post_type->name,
+					$number,
+					$name
+				);
+
+			// Otherwise just the count and post type name.
+			} else {
+				echo sprintf(
+					'<li class="post-count %1s-count">%2s %3s</li>',
+					$post_type->name,
+					$number,
+					$name
+				);
+
+			}
+		}
 	}
 }
---
+++ views/backend/widgets/welcome-panel.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Custom dashboard panel output
+ *
+ * @package    SPR_Core
+ * @subpackage Views
+ * @category   Widgets
+ * @since      1.0.0
+ */
+
+namespace SPR_Core;
+
+// Get the contact page ID.
+$contact_page = get_page_by_path( 'contact' );
+$contact_id   = '';
+if ( $contact_page ) {
+	$contact_id = $contact_page->ID;
+}
+
+// Get post type objects for labels & icons.
+$listing = get_post_type_object( 'listing' );
+$rental  = get_post_type_object( 'rental' );
+
+?>
+<div id="sprc-dashboard-website-content" class="sprc-dashboard-content-wrapper">
+
+	<header class="sprc-dashboard-section-header">
+		<h2><?php _e( 'Manage Website Content', 'spr-core' ); ?></h2>
+	</header>
+
+	<ul class="sprc-dashboard-actions">
+		<li>
+			<h3><?php _e( 'Images & Video', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon media-icon"><span class="dashicons dashicons-format-image"></span></div>
+			<p>
+				<a href="<?php echo admin_url( 'media-new.php' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'upload.php' ); ?>"><?php _e( 'Manage', 'spr-core' ); ?></a>
+			</p>
+		</li>
+		<li>
+			<h3><?php _e( 'Sale Listings', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon listing-icon"><span class="dashicons <?php echo $listing->menu_icon; ?>"></span></div>
+			<p>
+				<a href="<?php echo admin_url( 'post-new.php?post_type=listing' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'edit.php?post_type=listing' ); ?>"><?php _e( 'View List', 'spr-core' ); ?></a>
+			</p>
+		</li>
+		<li>
+			<h3><?php _e( 'Rental Listings', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon rental-icon"><span class="dashicons <?php echo $rental->menu_icon; ?>"></span></div>
+			<p>
+				<a href="<?php echo admin_url( 'post-new.php?post_type=rental' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'edit.php?post_type=rental' ); ?>"><?php _e( 'View List', 'spr-core' ); ?></a>
+			</p>
+		</li>
+		<li>
+			<h3><?php _e( 'Front Page', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon front-icon"><span class="dashicons dashicons-admin-home"></span></div>
+			<p>
+				<a href="<?php echo admin_url( 'post.php?post=' . get_option( 'page_on_front' ) . '&action=edit' ); ?>"><?php _e( 'Manage Content', 'spr-core' ); ?></a>
+			</p>
+		</li>
+	</ul>
+</div>
+
+<?php
+/**
+ * Content from the FlexMLS IDX plugin
+ *
+ * Checks to see if the plugin is active.
+ *
+ * @since 1.0.0
+ */
+if ( is_plugin_active( 'flexmls-idx/flexmls_connect.php' ) ) :
+
+?>
+<div id="sprc-dashboard-integrated-content" class="sprc-dashboard-content-wrapper">
+
+	<header class="sprc-dashboard-section-header">
+		<h2><?php _e( 'Manage Integrated Content', 'spr-core' ); ?></h2>
+	</header>
+
+	<ul class="sprc-dashboard-actions">
+		<li>
+			<h3><?php _e( 'IDX Integration', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon idx-widgets-icon"><span class="dashicons dashicons-migrate"></span></div>
+			<p>
+				<a href="<?php echo admin_url( 'widgets.php' ); ?>"><?php _e( 'Manage Widgets', 'spr-core' ); ?></a>
+			</p>
+		</li>
+		<li>
+			<h3><?php _e( 'IDX Neighborhoods', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon idx-neighborhoods-icon"><span class="dashicons dashicons-admin-multisite"></span></div>
+			<p>
+				<a href="<?php echo admin_url( 'admin.php?page=fmc_admin_neighborhood' ); ?>"><?php _e( 'Add Neighborhood', 'spr-core' ); ?></a>
+			</p>
+		</li>
+		<li>
+			<h3><?php _e( 'IDX Settings', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon idx-settings-icon"><span class="dashicons dashicons-admin-settings"></span></div>
+			<p>
+				<a href="<?php echo admin_url( 'admin.php?page=fmc_admin_settings' ); ?>"><?php _e( 'Manage Settings', 'spr-core' ); ?></a>
+			</p>
+		</li>
+		<?php if ( $contact_page ) : ?>
+		<li>
+			<h3><?php _e( 'Contact Page', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon contact-icon"><span class="dashicons dashicons-email"></span></div>
+			<p>
+				<a href="<?php echo admin_url( 'post.php?post=' . $contact_id . '&action=edit' ); ?>"><?php _e( 'Manage Info', 'spr-core' ); ?></a>
+			</p>
+		</li>
+		<?php endif; ?>
+	</ul>
+</div>
+<?php endif; ?>
