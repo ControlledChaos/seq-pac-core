265af5dbc8d58a08ae8e066355ff0f7ecd7188d7
ControlledChaos <greg@ccdzine.com>
2021-05-31 21:01:09 -0700

    Rework dashboard grid.

--- views/backend/widgets/welcome-panel.php
+++ views/backend/widgets/welcome-panel.php
@@ -11,35 +11,53 @@
 namespace SPR_Core;
 
 // Get post type objects for labels & icons.
-$listing = get_post_type_object( 'listing' );
-$rental  = get_post_type_object( 'rental' );
+$location = get_post_type_object( 'location' );
+$featured = get_post_type_object( 'featured' );
+$rental   = get_post_type_object( 'rental' );
+$page     = get_post_type_object( 'page' );
+
+/**
+ * Content from the FlexMLS MLS plugin
+ *
+ * Checks to see if the plugin is active.
+ *
+ * @since 1.0.0
+ */
+if (
+	is_plugin_active( 'flexmls-idx/flexmls_connect.php' ) ||
+	is_plugin_active( 'mls-idx/mls-idx.php' )
+) {
+	$mls_plugin = true;
+} else {
+	$mls_plugin = false;
+}
 
 ?>
-<div id="sprc-dashboard-website-content" class="sprc-dashboard-content-wrapper">
+<div id="sprc-dashboard-integrated-content" class="sprc-dashboard-content-wrapper">
 
 	<header class="sprc-dashboard-section-header">
-		<h2><?php _e( 'Manage Website Content', 'spr-core' ); ?></h2>
+		<h2><?php _e( 'Manage Website Pages', 'spr-core' ); ?></h2>
 	</header>
 
 	<ul class="sprc-dashboard-actions">
 		<li>
-			<h3><?php _e( 'Images & Video', 'spr-core' ); ?></h3>
-			<div class="sprc-dashboard-actions-icon media-icon"><span class="dashicons dashicons-format-image"></span></div>
+			<h3><?php echo $featured->labels->menu_name; ?></h3>
+			<div class="sprc-dashboard-actions-icon featured-icon"><span class="dashicons <?php echo $featured->menu_icon; ?>"></span></div>
 			<p>
-				<a href="<?php echo admin_url( 'media-new.php' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
-				<a href="<?php echo admin_url( 'upload.php' ); ?>"><?php _e( 'Manage', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'post-new.php?post_type=featured' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'edit.php?post_type=featured' ); ?>"><?php _e( 'Manage', 'spr-core' ); ?></a>
 			</p>
 		</li>
 		<li>
-			<h3><?php _e( 'Sale Listings', 'spr-core' ); ?></h3>
-			<div class="sprc-dashboard-actions-icon listing-icon"><span class="dashicons <?php echo $listing->menu_icon; ?>"></span></div>
+			<h3><?php echo $location->labels->menu_name; ?></h3>
+			<div class="sprc-dashboard-actions-icon location-icon"><span class="dashicons <?php echo $location->menu_icon; ?>"></span></div>
 			<p>
-				<a href="<?php echo admin_url( 'post-new.php?post_type=listing' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
-				<a href="<?php echo admin_url( 'edit.php?post_type=listing' ); ?>"><?php _e( 'Manage', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'post-new.php?post_type=location' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'edit.php?post_type=location' ); ?>"><?php _e( 'Manage', 'spr-core' ); ?></a>
 			</p>
 		</li>
 		<li>
-			<h3><?php _e( 'Rental Listings', 'spr-core' ); ?></h3>
+			<h3><?php echo $rental->labels->menu_name; ?></h3>
 			<div class="sprc-dashboard-actions-icon rental-icon"><span class="dashicons <?php echo $rental->menu_icon; ?>"></span></div>
 			<p>
 				<a href="<?php echo admin_url( 'post-new.php?post_type=rental' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
@@ -58,56 +76,42 @@ $rental  = get_post_type_object( 'rental' );
 	</ul>
 </div>
 
-<?php
-/**
- * Content from the FlexMLS MLS plugin
- *
- * Checks to see if the plugin is active.
- *
- * @since 1.0.0
- */
-if (
-	is_plugin_active( 'flexmls-idx/flexmls_connect.php' ) ||
-	is_plugin_active( 'mls-idx/mls-idx.php' )
-) :
-
-?>
-<div id="sprc-dashboard-integrated-content" class="sprc-dashboard-content-wrapper">
+<div id="sprc-dashboard-website-content" class="sprc-dashboard-content-wrapper">
 
 	<header class="sprc-dashboard-section-header">
-		<h2><?php _e( 'Manage Integrated Content', 'spr-core' ); ?></h2>
+		<h2><?php _e( 'Manage Website Content', 'spr-core' ); ?></h2>
 	</header>
 
 	<ul class="sprc-dashboard-actions">
 		<li>
-			<h3><?php _e( 'MLS Integration', 'spr-core' ); ?></h3>
-			<div class="sprc-dashboard-actions-icon idx-widgets-icon"><span class="dashicons dashicons-migrate"></span></div>
+			<h3><?php _e( 'Media Library', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon media-icon"><span class="dashicons dashicons-format-image"></span></div>
+			<p>
+				<a href="<?php echo admin_url( 'media-new.php' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'upload.php' ); ?>"><?php _e( 'Manage', 'spr-core' ); ?></a>
+			</p>
+		</li>
+		<li>
+			<h3><?php _e( 'Content Widgets', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon widgets-icon"><span class="dashicons dashicons-screenoptions"></span></div>
 			<p>
 				<a href="<?php echo admin_url( 'widgets.php' ); ?>"><?php _e( 'Manage Widgets', 'spr-core' ); ?></a>
 			</p>
 		</li>
 		<li>
-			<h3><?php _e( 'MLS Neighborhoods', 'spr-core' ); ?></h3>
-			<div class="sprc-dashboard-actions-icon idx-neighborhoods-icon"><span class="dashicons dashicons-admin-multisite"></span></div>
+			<h3><?php _e( 'Navigation Menus', 'spr-core' ); ?></h3>
+			<div class="sprc-dashboard-actions-icon nav-menus-icon"><span class="dashicons dashicons-menu-alt"></span></div>
 			<p>
-				<a href="<?php echo admin_url( 'admin.php?page=fmc_admin_neighborhood' ); ?>"><?php _e( 'Add Neighborhood', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'nav-menus.php' ); ?>"><?php _e( 'Manage Widgets', 'spr-core' ); ?></a>
 			</p>
 		</li>
 		<li>
-			<h3><?php _e( 'MLS Settings', 'spr-core' ); ?></h3>
-			<div class="sprc-dashboard-actions-icon idx-settings-icon"><span class="dashicons dashicons-admin-settings"></span></div>
+			<h3><?php echo $page->labels->menu_name; ?></h3>
+			<div class="sprc-dashboard-actions-icon pages-icon"><span class="dashicons <?php echo $page->menu_icon; ?>"></span></div>
 			<p>
-				<a href="<?php echo admin_url( 'admin.php?page=fmc_admin_settings' ); ?>"><?php _e( 'Manage Settings', 'spr-core' ); ?></a>
-			</p>
-		</li>
-		<li>
-			<h3><?php _e( 'MLS Plugin Support', 'spr-core' ); ?></h3>
-			<div class="sprc-dashboard-actions-icon contact-icon"><span class="dashicons dashicons-welcome-learn-more"></span></div>
-			<p>
-				<a href="<?php echo esc_url( 'https://fbsidx.com/plugin/examples/' ); ?>" target="_blank" rel="noopener noreferrer nofollow"><?php _e( 'Examples', 'spr-core' ); ?></a>
-				<a href="<?php echo esc_url( 'https://fbsidx.com/plugin/documentation/' ); ?>" target="_blank" rel="noopener noreferrer nofollow"><?php _e( 'Documentation', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'post-new.php?post_type=page' ); ?>"><?php _e( 'Add New', 'spr-core' ); ?></a>
+				<a href="<?php echo admin_url( 'edit.php?post_type=page' ); ?>"><?php _e( 'Manage', 'spr-core' ); ?></a>
 			</p>
 		</li>
 	</ul>
 </div>
-<?php endif; ?>
